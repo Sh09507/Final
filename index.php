d18d4991078c76559e78923cd7ae4a8c0ec9bf6f
Sh09507 <71281402+Sh09507@users.noreply.github.com>
2020-11-30 20:25:12 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<!-- Sabrina Hill
+	 Final 
+	 Fall 2020
+-->
+<head>
+	<title>Sabrina Hill Final Project</title>
+	<meta charset= "utf-8">
+	<!--<link href="styles.css" rel="stylesheet" />-->
+</head>
+<body>
+	<h1>Hex to RGB Color Converter</h1>
+	<form action="" method="POST" id="">
+		<fieldset id="contact">
+		<legend>Please enter 6 digits hex color code</legend>
+			 
+			<label for="titlebox">Hex color code:</label>
+			<input type="text" name="hex" id="hexbox">
+		</fieldset>
+	</form>
+</body>
+</html>
